1615e97d48b79638acce1f803bdc22d740b7f4bb
Alessandro <zproale@gmail.com>
2020-01-13 19:00:31 +0100

    Aggiunti i vari esempi

--- example.php
+++ example.php
@@ -1,5 +1,14 @@
 <?php
+
 include("classeviva_class.php");
 $classeviva = new Classeviva();
-echo $classeviva->view('NUMERO VIDEO','PHPSESSONID');
+$session = "PHPSESSIONID";
+echo $session;
+echo $classeviva->minuti_slide($session);
+echo $classeviva->minuti_normative($session);
+echo $classeviva->minuti_video($session);
+echo $classeviva->minuti_test($session);
+echo $classeviva->view("NUMERO VIDEO",$session);
+
+
 ?>
